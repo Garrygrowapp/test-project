c1749f0ca263685dd5e75475c419a082214c7454
Garry_growapp <yugstsigrey@gmail.com>
2021-12-13 23:34:40 +0300

    english_words_random

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Garry app',
-      theme: ThemeData(
-        fontFamily: 'Georgia',
-        primarySwatch: Colors.lightGreen,
-        textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          //child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
-      home: HomePage(),
     );
   }
 }
