37ab4a321f78b40db93c87ad00b89941527fd4c2
Garry_growapp <yugstsigrey@gmail.com>
2021-12-24 16:52:15 +0300

    add_SavedScreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Random Words',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF00FFD1),
+          foregroundColor: Color(0xFF23262C),
+        ),
+      ),
       home: RandomWords(),
     );
   }
@@ -60,7 +66,7 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold (                     // Add from here...
+    return Scaffold (
       appBar: AppBar(
         title: const Text('Garry app'),
         actions: [
@@ -72,21 +78,13 @@ class _RandomWordsState extends State<RandomWords> {
         ],
       ),
       body: _buildSuggestions(),
-    );                                      // ... to here.
+    );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
         itemBuilder: (BuildContext _context, int i) {
-          // Add a one-pixel-high divider widget before each row
-          // in the ListView.
           if (i.isOdd) {
             return Divider();
           }
